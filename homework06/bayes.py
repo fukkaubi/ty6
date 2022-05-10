8f07cb17d6b912b11b4700f3d287a2509fdce609
fukkaubi <105294291+fukkaubi@users.noreply.github.com>
2022-05-10 16:32:57 +0300

    Add files via upload

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,17 +1,97 @@
+import itertools
+import math
+import operator
+import typing as tp
+from collections import Counter, defaultdict
+from copy import deepcopy
+
+
 class NaiveBayesClassifier:
+    def __init__(self, alpha: float) -> None:
+        if not (alpha <= 1.0 and alpha > 0.0):
+            raise ValueError("Smoothing parameter must be between 0.0 and 1.0")
+        self.priors: tp.Counter[str] = Counter()
+        self.alpha: float = alpha
+        self.doc_count: int = 0
+        self.unique_words: tp.List[str] = []
+        self.words_per_class: tp.DefaultDict[str, tp.DefaultDict[str, float]] = defaultdict(
+            defaultdict
+        )
+        self.class_lengths: tp.DefaultDict[str, float] = defaultdict(float)
+        self.word_probs: tp.DefaultDict[str, tp.DefaultDict[str, float]] = defaultdict(defaultdict)
 
-    def __init__(self, alpha):
-        pass
-
-    def fit(self, X, y):
+    def fit(self, X: tp.List[str], y: tp.List[str]) -> None:
         """ Fit Naive Bayes classifier according to X, y. """
-        pass
+        if not X or not y or not (len(X) == len(y)):
+            raise ValueError("The training set is either unmarked or empty.")
+        self.doc_count = len(X)
+        self.priors = Counter(y)
+        for e in self.priors:
+            self.priors[e] /= len(y)  # type: ignore
+        unique_words_prepare = [i.split(" ") for i in X]
+        self.unique_words = list(itertools.chain.from_iterable(unique_words_prepare))
+        self.unique_words = sorted(list(set(self.unique_words)))
+        self.words_per_class = defaultdict(defaultdict)
+        self.class_lengths = defaultdict(float)
+        for i, string in enumerate(X):
+            words = string.split(" ")
+            c = y[i]
+            self.class_lengths[c] += len(words)
+            for word in words:
+                if not word in self.words_per_class.keys():
+                    self.words_per_class[word] = defaultdict(
+                        float,
+                        {
+                            key: value
+                            for (key, value) in zip(list(set(y)), [0 for _ in list(set(y))])
+                        },
+                    )
+                self.words_per_class[word][c] += 1
+        self.word_probs = deepcopy(self.words_per_class)
+        for word, value in self.word_probs.items():
+            for c, _ in value.items():
+                self.word_probs[word][c] = (self.word_probs[word][c] + self.alpha) / (
+                    self.class_lengths[c] + self.alpha * len(self.unique_words)
+                )
 
-    def predict(self, X):
+    def predict(self, X: tp.List[str]) -> tp.List[str]:
         """ Perform classification on an array of test vectors X. """
-        pass
+        if (
+            not self.priors
+            or not self.doc_count
+            or not self.unique_words
+            or not self.words_per_class
+            or not self.class_lengths
+            or not self.word_probs
+        ):
+            raise ValueError("The model is untrained")
+        labels = []
+        for document in X:
+            words = document.split(" ")
+            probabilities: tp.DefaultDict[str, float] = defaultdict(float)
+            for c in self.class_lengths.keys():
+                word_probs = []
+                for word in words:
+                    if word in self.unique_words:
+                        word_probs.append(math.log(self.word_probs[word][c]))
+                word_probs_sum = sum(word_probs)
+                probabilities[c] = math.log(self.priors[c]) + word_probs_sum
+            predicted_label = max(probabilities.items(), key=operator.itemgetter(1))[0]
+            labels.append(predicted_label)
+        return labels
 
-    def score(self, X_test, y_test):
+    def score(self, X_test: tp.List[str], y_test: tp.List[str]) -> float:
         """ Returns the mean accuracy on the given test data and labels. """
-        pass
-
+        predicted = self.predict(X_test)
+        class_accuracies = defaultdict(float)
+        for c in list(set(y_test)):
+            if y_test.count(c):
+                true_positives = sum(
+                    [1 for i, e in enumerate(predicted) if e == c and y_test[i] == c]
+                )
+                false_negatives = sum(
+                    [1 for i, e in enumerate(predicted) if e != c and y_test[i] == c]
+                )
+                class_accuracies[c] = true_positives / (true_positives + false_negatives)
+        score = sum([i for i in class_accuracies.values()]) / len(list(set(y_test)))
+        return score
--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -1,32 +1,51 @@
+import typing as tp
+
 import requests
 from bs4 import BeautifulSoup
 
 
-def extract_news(parser):
+def extract_news(parser: BeautifulSoup) -> tp.List[tp.Dict[str, tp.Any]]:
     """ Extract news from a given web page """
     news_list = []
+    news_table = parser.find_all("tr", {"class": "athing"})
+    info = parser.find_all("td", {"class": "subtext"})
+    for i in range(len(news_table)):
+        comments = "0"
+        links = info[i].find_all("a")
+        title = news_table[i].find(class_="storylink")
+        points = info[i].span.text.split()[0]
+        print(links[-1].text)
+        if links[-1].text != "discuss":
+            comments = links[-1].text.split()[0]
 
-    # PUT YOUR CODE HERE
+        news = {
+            "author": links[0].text,
+            "comments": comments,
+            "points": points,
+            "title": title.text,
+            "url": title["href"],
+            "label": None,
+        }
+        news_list.append(news)
 
     return news_list
 
 
-def extract_next_page(parser):
+def extract_next_page(parser: BeautifulSoup) -> tp.Any:
     """ Extract next page URL """
-    # PUT YOUR CODE HERE
+    return parser.table.find_all("table")[1].find_all("a")[-1]["href"]
 
 
-def get_news(url, n_pages=1):
+def get_news(url: str, n_pages: int = 1) -> tp.List[tp.Dict[str, tp.Any]]:
     """ Collect news from a given web page """
     news = []
     while n_pages:
         print("Collecting data from page: {}".format(url))
         response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
+        soup = BeautifulSoup(response.text, "html5lib")
         news_list = extract_news(soup)
         next_page = extract_next_page(soup)
         url = "https://news.ycombinator.com/" + next_page
         news.extend(news_list)
         n_pages -= 1
     return news
-
